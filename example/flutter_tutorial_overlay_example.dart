9d0fd84ccc0111274f432a7b93f9a58384ddbd9f
Ali Al-Zaidy <ali.route9@gmail.com>
2025-08-25 09:52:53 +0300

    first commit

---
+++ example/flutter_tutorial_overlay_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_tutorial_overlay/flutter_tutorial_overlay.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_tutorial_overlay_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_tutorial_overlay/flutter_tutorial_overlay.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
