c998f096af422fc6f827d6e58e1d32bcea0356d0
Ali Al-Zaidy <ali.route9@gmail.com>
2025-09-08 12:14:42 +0300

    just formatting

--- lib/flutter_tutorial_overlay.dart
+++ lib/flutter_tutorial_overlay.dart
@@ -1,5 +1,5 @@
 /// A Flutter package for creating interactive app tutorials and onboarding experiences.
-/// 
+///
 /// This library provides widgets and utilities to create step-by-step tutorials
 /// that highlight specific UI elements with customizable tooltips and overlays.
 library flutter_tutorial_overlay;
